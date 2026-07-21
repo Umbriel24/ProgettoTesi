e89ff58932a6b8d162d1d5eaa67ace93ea4e6398
Umberto <87665468+Umbriel24@users.noreply.github.com>
2026-07-21 20:12:45 +0000

    fix main per renderlo universale

--- src/main.py
+++ src/main.py
@@ -4,59 +4,70 @@ from csvutility import utility_csv
 
 
 if __name__ == "__main__":
-    print("Scrivi il numero per continuare l'esecuzione")
-    print("1: Cerca il miglior modello tra le reti")
-    print("2: Testa tutti i modelli")
+
+    def main(num: int):
+        
+        print("Scrivi il numero per continuare l'esecuzione")
+        print("1: Cerca il miglior modello tra le reti")
+        print("2: Testa tutti i modelli")
 
 
-    num = 11
-    if int(num) == 1:
-        utility_csv.trova_miglior_percentage()
-    elif int(num) == 2:
-        for i in range(9):
-            TestModello(f"model_resnet18_percentage{i*5}.pth", 777)
+        if int(num) == 1:
+            utility_csv.trova_miglior_percentage()
+        elif int(num) == 2:
+            for i in range(9):
+                TestModello(f"model_resnet18_percentage{i*5}.pth", 777)
+        else:
+
+
+            #create_and_train_model("resnet18", pre_trained_value=True, percentage_drop=0)
+            #create_and_train_model("resnet18", pre_trained_value=True, percentage_drop=5)
+            #create_and_train_model("resnet18", pre_trained_value=True, percentage_drop=10)
+            #create_and_train_model("resnet18", pre_trained_value=True, percentage_drop=15)
+            #create_and_train_model("resnet18", pre_trained_value=True, percentage_drop=20)
+            create_and_train_model("resnet18", pre_trained_value=True, percentage_drop=25)
+            create_and_train_model("resnet18", pre_trained_value=True, percentage_drop=30)
+            create_and_train_model("resnet18", pre_trained_value=True, percentage_drop=35)
+            create_and_train_model("resnet18", pre_trained_value=True, percentage_drop=40)
+
+            create_and_train_model("densenet", pre_trained_value=True, percentage_drop=0)
+            create_and_train_model("densenet", pre_trained_value=True, percentage_drop=5)
+            create_and_train_model("densenet", pre_trained_value=True, percentage_drop=10)
+            create_and_train_model("densenet", pre_trained_value=True, percentage_drop=15)
+            create_and_train_model("densenet", pre_trained_value=True, percentage_drop=20)
+            create_and_train_model("densenet", pre_trained_value=True, percentage_drop=25)
+            create_and_train_model("densenet", pre_trained_value=True, percentage_drop=30)
+            create_and_train_model("densenet", pre_trained_value=True, percentage_drop=35)
+            create_and_train_model("densenet", pre_trained_value=True, percentage_drop=40)
+
+            create_and_train_model("resnet50", pre_trained_value=True, percentage_drop=0)
+            create_and_train_model("resnet50", pre_trained_value=True, percentage_drop=5)
+            create_and_train_model("resnet50", pre_trained_value=True, percentage_drop=10)
+            create_and_train_model("resnet50", pre_trained_value=True, percentage_drop=15)
+            create_and_train_model("resnet50", pre_trained_value=True, percentage_drop=20)
+            create_and_train_model("resnet50", pre_trained_value=True, percentage_drop=25)
+            create_and_train_model("resnet50", pre_trained_value=True, percentage_drop=30)
+            create_and_train_model("resnet50", pre_trained_value=True, percentage_drop=35)
+            create_and_train_model("resnet50", pre_trained_value=True, percentage_drop=40)
+
+            create_and_train_model("efficientnet", pre_trained_value=True, percentage_drop=0)
+            create_and_train_model("efficientnet", pre_trained_value=True, percentage_drop=5)
+            create_and_train_model("efficientnet", pre_trained_value=True, percentage_drop=10)
+            create_and_train_model("efficientnet", pre_trained_value=True, percentage_drop=15)
+            create_and_train_model("efficientnet", pre_trained_value=True, percentage_drop=20)
+            create_and_train_model("efficientnet", pre_trained_value=True, percentage_drop=25)
+            create_and_train_model("efficientnet", pre_trained_value=True, percentage_drop=30)
+            create_and_train_model("efficientnet", pre_trained_value=True, percentage_drop=35)
+            create_and_train_model("efficientnet", pre_trained_value=True, percentage_drop=40)
+
+def train_from_macrodrop(subnet_name: str, percentagedrop: int):
+    if i >= 9:
+        return
     else:
+        i = percentagedrop
 
-
-        #create_and_train_model("resnet18", pre_trained_value=True, percentage_drop=0)
-        #create_and_train_model("resnet18", pre_trained_value=True, percentage_drop=5)
-        #create_and_train_model("resnet18", pre_trained_value=True, percentage_drop=10)
-        #create_and_train_model("resnet18", pre_trained_value=True, percentage_drop=15)
-        #create_and_train_model("resnet18", pre_trained_value=True, percentage_drop=20)
-        create_and_train_model("resnet18", pre_trained_value=True, percentage_drop=25)
-        create_and_train_model("resnet18", pre_trained_value=True, percentage_drop=30)
-        create_and_train_model("resnet18", pre_trained_value=True, percentage_drop=35)
-        create_and_train_model("resnet18", pre_trained_value=True, percentage_drop=40)
-
-        create_and_train_model("densenet", pre_trained_value=True, percentage_drop=0)
-        create_and_train_model("densenet", pre_trained_value=True, percentage_drop=5)
-        create_and_train_model("densenet", pre_trained_value=True, percentage_drop=10)
-        create_and_train_model("densenet", pre_trained_value=True, percentage_drop=15)
-        create_and_train_model("densenet", pre_trained_value=True, percentage_drop=20)
-        create_and_train_model("densenet", pre_trained_value=True, percentage_drop=25)
-        create_and_train_model("densenet", pre_trained_value=True, percentage_drop=30)
-        create_and_train_model("densenet", pre_trained_value=True, percentage_drop=35)
-        create_and_train_model("densenet", pre_trained_value=True, percentage_drop=40)
-
-        create_and_train_model("resnet50", pre_trained_value=True, percentage_drop=0)
-        create_and_train_model("resnet50", pre_trained_value=True, percentage_drop=5)
-        create_and_train_model("resnet50", pre_trained_value=True, percentage_drop=10)
-        create_and_train_model("resnet50", pre_trained_value=True, percentage_drop=15)
-        create_and_train_model("resnet50", pre_trained_value=True, percentage_drop=20)
-        create_and_train_model("resnet50", pre_trained_value=True, percentage_drop=25)
-        create_and_train_model("resnet50", pre_trained_value=True, percentage_drop=30)
-        create_and_train_model("resnet50", pre_trained_value=True, percentage_drop=35)
-        create_and_train_model("resnet50", pre_trained_value=True, percentage_drop=40)
-
-        create_and_train_model("efficientnet", pre_trained_value=True, percentage_drop=0)
-        create_and_train_model("efficientnet", pre_trained_value=True, percentage_drop=5)
-        create_and_train_model("efficientnet", pre_trained_value=True, percentage_drop=10)
-        create_and_train_model("efficientnet", pre_trained_value=True, percentage_drop=15)
-        create_and_train_model("efficientnet", pre_trained_value=True, percentage_drop=20)
-        create_and_train_model("efficientnet", pre_trained_value=True, percentage_drop=25)
-        create_and_train_model("efficientnet", pre_trained_value=True, percentage_drop=30)
-        create_and_train_model("efficientnet", pre_trained_value=True, percentage_drop=35)
-        create_and_train_model("efficientnet", pre_trained_value=True, percentage_drop=40)
+        for i in range(9):
+            create_and_train_model("subnet_name", pre_trained_value=True, percentage_drop=(i*5))
 
 
 
