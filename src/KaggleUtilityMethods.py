5043268bb5bd20fa2cdfabd8c7376831f6b79173
Umbriel24 <umberto.gargi24@gmail.com>
2026-07-26 13:16:39 +0200

    Funzione per salvare i csv

--- src/KaggleUtilityMethods.py
+++ src/KaggleUtilityMethods.py
@@ -14,4 +14,26 @@ def zippa_file_kaggle():
     archive.close()
 
 
+
+# Prende tutit i file nella cartella persistenza. Se iniziano con report,
+# li inseriscen nello zip.
+def zippa_report_file_kaggle():
+    archive_path = config.PERSISTANCE_PATH
+    archive = zipfile.ZipFile("KaggleReports.zip", 'a')
+    for file in archive_path.iterdir():
+        if file.name.startswith("report"):
+            archive.write(file, compress_type=zipfile.ZIP_DEFLATED)
+
+    archive.close()
+
+
+def zippa_all_csv_file_kaggle():
+    archive_path = config.PERSISTANCE_PATH
+    archive = zipfile.ZipFile("KaggleCSV.zip", 'a')
+
+    for file in archive_path.iterdir():
+        if file.name.endswith(".csv"):
+            archive.write(file, compress_type=zipfile.ZIP_DEFLATED)
+
+
 zippa_file_kaggle()
